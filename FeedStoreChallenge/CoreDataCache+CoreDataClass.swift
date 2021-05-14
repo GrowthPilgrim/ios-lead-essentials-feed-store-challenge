5fda7fe315ee256a8cba17a16d56c76606ee6916
Dennis Nehrenheim <dennisnehrenheim90@gmail.com>
2021-05-14 12:04:48 +0200

    Create Core Data Model `FeedStore`

---
+++ FeedStoreChallenge/CoreDataCache+CoreDataClass.swift
@@ -0,0 +1,14 @@
+//
+//  CoreDataCache+CoreDataClass.swift
+//  FeedStoreChallenge
+//
+//  Created by Dennis Nehrenheim on 14.05.21.
+//  Copyright © 2021 Essential Developer. All rights reserved.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(CoreDataCache)
+public class CoreDataCache: NSManagedObject {}
---
+++ FeedStoreChallenge/CoreDataFeedImage+CoreDataClass.swift
@@ -0,0 +1,14 @@
+//
+//  CoreDataFeedImage+CoreDataClass.swift
+//  FeedStoreChallenge
+//
+//  Created by Dennis Nehrenheim on 14.05.21.
+//  Copyright © 2021 Essential Developer. All rights reserved.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(CoreDataFeedImage)
+public class CoreDataFeedImage: NSManagedObject {}
